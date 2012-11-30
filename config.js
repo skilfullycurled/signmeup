8c3d88604d151bc6635d4c2cd1afdd787cd0f2e4
Benj. N. Sugar <git@skilfullycurled.org>
2012-11-29 20:42:58 -0600

    nope, that didn't work, resetting .gitignore

---
+++ config.js
@@ -0,0 +1,13 @@
+var config = {}
+
+//MONGODB CONFIG
+config.mongodb = {};
+config.mongodb.host = 'http://localhost/YOUR MONGO DB'
+
+//TWILIO CONFIG
+config.twilio = {};
+config.twilio.accountSID = "YOUR TWILIO ACCOUNT SID";
+config.twilio.authToken = "YOUR TWILIO ACCOUNT AUTHTOKEN";
+config.twilio.number = "YOUR TWILIO NUMBER";
+
+module.exports = config;
